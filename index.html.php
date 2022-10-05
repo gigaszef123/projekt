be41645a2b8cb8ea791a2c85ce4a163ff65b9674
dt19cedrok <miedz.gora.krystian.cedro@gmail.com>
2022-10-05 09:17:47 +0200

    jd

---
+++ index.html.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$title="Strona Główna";
+echo $title;
+
+
+?>
+<ol>
+    <p><li><a href="planlekcji.php">Plan Lekcji</a></li></p>
+    <p><li><a href="syntax.php">echo</a></li></p>
+</ol>
+</body>
+</html>
+<?php
