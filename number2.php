9b2562404563b615d27c766fa8ab4c7347a76ddd
dt19cedrok <miedz.gora.krystian.cedro@gmail.com>
2022-10-05 10:52:16 +0200

    xd

---
+++ number2.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+<?php
+$x= 5;
+$y= 15;
+$o= $x + $y;
+$a= $x - $y;
+$b= $x * $y;
+$c= $x / $y;
+echo "wynik dodawnia ".$x." i ".$y." wynosi ".$o;
+echo "<br>";
+echo "wynik odejmowania ".$x." i ".$y." wynosi ".$a;
+echo "<br>";
+echo "wynik mnożenia ".$x." i ".$y." wynosi ".$b;
+echo "<br>";
+echo "wynik dzielenia ".$x." i ".$y. " wynosi ".$c;
+
+$d = 4;
+$e = 35;
+$P = $d * $e;
+$F = 2*$d + 2*$e;
+$g = $d % $e;
+echo "<br>";
+echo "Pole prostokoąta o bokach ".$d." i ".$e." wynosi ".$P;
+echo "<br>";
+echo "Obwód prostokąta wynosi ".$d." i ".$e." wynosi ".$F;
+echo "<br>";
+echo "Reszta z dzielenia ".$d." i ".$e." wynosi ".$g;
+
+
+
+
+?>
+</body>
+</html>
