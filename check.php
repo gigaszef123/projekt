43f130f9ea9a16e723e81192d87a5048b41e7cae
dt19cedrok <miedz.gora.krystian.cedro@gmail.com>
2023-03-01 11:15:30 +0100

    aaaa

---
+++ check.php
@@ -0,0 +1,42 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        fieldset {
+            border: green solid ;
+            padding: 3px;
+            width: 500px;
+            height: 100px;
+            border-width: 3px;
+
+        }
+    </style>
+</head>
+<body>
+<form action="check.php" method="post">
+
+    <fieldset>
+        <legend>Liczby</legend>
+        <?php
+        foreach (range(0, 100, 10) as $value) {
+            echo "<label for='nr'><input type='checkbox' name='aha' id='a' ></label> $value";
+        }
+        ?>
+        <br>
+        <input type="submit" value="Wykonaj">
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST')
+        {
+            echo '<pre>';
+            Print_r($_POST);
+            echo '</pre>';
+        }
+        ?>
+    </fieldset>
+</form>
--- loremaha.php
+++ loremaha.php
@@ -1,35 +1,46 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Formularz z polem tekstowym</title>
     <style>
         fieldset {
             border: 2px solid black;
             padding: 10px;
         }
-        .red {
-            font-style: italic;
+        .czer {
             color: red;
+            font-style: italic;
         }
     </style>
 </head>
 <body>
 
-<form method="post">
-    <fieldset>
-        <legend>lorem50</legend>
-        <label>Tekst:</label><br>
-        <textarea name="tekst" rows="5" cols="50">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id purus vel quam malesuada blandit vel non enim. Etiam vulputate, sapien sit amet facilisis laoreet, tellus velit tristique arcu, a lacinia urna justo eu ipsum. Fusce vitae tortor sed augue sagittis facilisis a at augue. In hac habitasse platea dictumst. Aenean euismod, ante id convallis bibendum, dui est faucibus augue, vel sollicitudin sapien justo sed lorem. Maecenas porttitor mi ut nunc sagittis, sit amet mollis risus congue. Ut a lectus ut arcu pretium varius. Sed sed lobortis nisl, sed dapibus risus.</textarea><br>
-        <input type="submit" value="Wyślij">
-    </fieldset>
-</form>
+
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $tekst = $_POST["tekst"];
-    echo '<fieldset><legend>lorem50</legend><p>Oto wpisany tekst:</p><p class="red">'.$tekst.'</p></fieldset>';
+    echo '<fieldset><legend>lorem50</legend><p>Oto wpisany tekst:</p><p class="czer">'.$tekst.'</p></fieldset>';
+}
+
+else
+{
+?>
+    <form method="post">
+        <fieldset>
+            <legend>lorem50</legend>
+            <label>Tekst:</label><br>
+            <textarea name="tekst" rows="5" cols="50">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id purus vel quam malesuada blandit vel non enim. Etiam vulputate, sapien sit amet facilisis laoreet, tellus velit tristique arcu, a lacinia urna justo eu ipsum. Fusce vitae tortor sed augue sagittis facilisis a at augue. In hac habitasse platea dictumst. Aenean euismod, ante id convallis bibendum, dui est faucibus augue, vel sollicitudin sapien justo sed lorem. Maecenas porttitor mi ut nunc sagittis, sit amet mollis risus congue. Ut a lectus ut arcu pretium varius. Sed sed lobortis nisl, sed dapibus risus.</textarea><br>
+            <input type="submit" value="Wyślij">
+        </fieldset>
+    </form>
+
+<?php
 }
 ?>
+
+
+
+
 
 </body>
 </html>
